e644adcb6387a845600b067171a9664012ba0b3c
Jon-Paul Lussier Jr <jonpaul.lussier@gmail.com>
2011-03-24 00:43:04 -0400

    To koan 155 -- triangles are funrake

--- triangle.rb
+++ triangle.rb
@@ -14,9 +14,29 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  
+  #Triangles require that the sum of two sides is less than the remaining side
+  if a+b <= c || a+c <= b || c+b <= a
+    raise TriangleError, "Tryangle again."
+  end
+  
+  # Why didn't OR statement work here? Why can't we ask, if A or B or C == 0 raise -- how to say this?
+  if a == 0 && b == 0 && c == 0
+    raise TriangleError, "Tryangle again."
+  end
+  
+  if a == b && a == c && b == c
+    return :equilateral
+  end
+  
+  if a == b && a != c || c == b && c != a || a == c && a != b
+    return :isosceles
+  end
+  
+  if a != b && a != c && b != c
+    return :scalene
+  end
 end
 
-# Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
 end
